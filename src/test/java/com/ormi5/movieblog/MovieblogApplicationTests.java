a04ececd3183d189b3e29f08d2addd7b15eb0286
flarin <flintintoe@gmail.com>
2024-07-22 09:47:35 +0900

    Create base files

---
+++ src/test/java/com/ormi5/movieblog/MovieblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ormi5.movieblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
