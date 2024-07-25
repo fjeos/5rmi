518cc8010d44838a47d16ac9af3c453112a7d7be
duys2 <minigun5@naver.com>
2024-07-25 23:42:00 +0900

    [Fix] 버그 수정

--- src/main/java/com/ormi5/movieblog/comment/CommentDto.java
+++ src/main/java/com/ormi5/movieblog/comment/CommentDto.java
@@ -1,6 +1,7 @@
 package com.ormi5.movieblog.comment;
 
 import com.ormi5.movieblog.post.Post;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,41 +10,38 @@ import java.time.Instant;
 @Getter
 @Builder
 public class CommentDto {
-    private Long id;
-    private Long postId;
-    private Long userId;
-    private String author;
-    private String content;
-    private int likes;
-    private int dislikes;
-    private Instant createAt;
-    private Instant updateAt;
+	private Long id;
+	private Long postId;
+	private Long userId;
+	private String content;
+	private int likes;
+	private int dislikes;
+	private Instant createAt;
+	private Instant updateAt;
 
-    public static CommentDto toDto(Comment comment) {
-        return CommentDto.builder()
-                .id(comment.getCommentId())
-                .postId(comment.getPost().getPostId())
-                .userId(comment.getUserId())
-                .author(comment.getAuthor())
-                .content(comment.getContent())
-                .likes(comment.getLikes())
-                .dislikes(comment.getDislikes())
-                .createAt(comment.getCreateAt())
-                .updateAt(comment.getUpdateAt())
-                .build();
-    }
+	public static CommentDto toDto(Comment comment) {
+		return CommentDto.builder()
+			.id(comment.getCommentId())
+			.postId(comment.getPost().getPostId())
+			.userId(comment.getUserId())
+			.content(comment.getContent())
+			.likes(comment.getLikes())
+			.dislikes(comment.getDislikes())
+			.createAt(comment.getCreateAt())
+			.updateAt(comment.getUpdateAt())
+			.build();
+	}
 
-    public static Comment toEntity(CommentDto commentDto, Post post) {
-        return Comment.builder()
-                .commentId(commentDto.getId())
-                .post(post) // post 필드를 CommentDto에서 직접 설정
-                .userId(commentDto.getUserId())
-                .author(commentDto.getAuthor())
-                .content(commentDto.getContent())
-                .likes(commentDto.getLikes())
-                .dislikes(commentDto.getDislikes())
-                .createAt(Instant.now())
-                .updateAt(Instant.now())
-                .build();
-    }
+	public static Comment toEntity(CommentDto commentDto, Post post) {
+		return Comment.builder()
+			.commentId(commentDto.getId())
+			.post(post) // post 필드를 CommentDto에서 직접 설정
+			.userId(commentDto.getUserId())
+			.content(commentDto.getContent())
+			.likes(commentDto.getLikes())
+			.dislikes(commentDto.getDislikes())
+			.createAt(Instant.now())
+			.updateAt(Instant.now())
+			.build();
+	}
 }
