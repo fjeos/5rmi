a1527a0739bf387ac520cfe3a8684ad6e773405a
duys2 <minigun5@naver.com>
2024-07-25 18:23:43 +0900

    [Feat] 댓글 조회

--- src/main/java/com/ormi5/movieblog/commentcontroller/CommentController.java
+++ src/main/java/com/ormi5/movieblog/commentcontroller/CommentController.java
@@ -1,5 +1,8 @@
 package com.ormi5.movieblog.commentcontroller;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.ormi5.movieblog.comment.Comment;
 import com.ormi5.movieblog.comment.CommentDto;
 
@@ -19,43 +22,58 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
-    private final CommentService commentService;
-    private final CommentRepository commentRepository;
+	private final CommentService commentService;
+	private final CommentRepository commentRepository;
 
-    @Autowired
-    public CommentController(CommentService commentService, CommentRepository commentRepository) {
-        this.commentService = commentService;
-        this.commentRepository = commentRepository;
-    }
+	@Autowired
+	public CommentController(CommentService commentService, CommentRepository commentRepository) {
+		this.commentService = commentService;
+		this.commentRepository = commentRepository;
+	}
 
-    @PostMapping
-    public ResponseEntity<CommentDto> addComment(@RequestBody CommentDto commentDto) {
-        CommentDto createComment = commentService.createComment(commentDto);
+	@PostMapping
+	public ResponseEntity<CommentDto> addComment(@RequestBody CommentDto commentDto) {
+		CommentDto createComment = commentService.createComment(commentDto);
 
-        return new ResponseEntity<>(createComment, HttpStatus.CREATED);
-    }
+		return new ResponseEntity<>(createComment, HttpStatus.CREATED);
+	}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteComment(@PathVariable Long id, @RequestBody DeleteCommentDto deleteCommentDto) {
-        Comment comment = commentRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Comment not found"));
+	@GetMapping
+	public ResponseEntity<List<CommentDto>> getAllComments() {
+		List<CommentDto> commentDtoList = commentService.getAllComments();
 
-        if (!comment.getUserId().equals(deleteCommentDto.getUserId())) {
-            return ResponseEntity.badRequest().body("User not authorized to delete this comment");
-        }
+		return ResponseEntity.ok(commentDtoList);
+		// return new ResponseEntity<>(commentDtoList, HttpStatus.OK);
+	}
 
-        commentRepository.delete(comment);
-        return ResponseEntity.ok().build();
-    }
+	@GetMapping("/{postId}")
+	public ResponseEntity<List<CommentDto>> getCommentByPostId(@PathVariable Long postId) {
+		List<CommentDto> commentDtoList = commentService.getCommentsByPostId(postId);
 
-    /**
-     * 댓글 수정 controller
-     * @param commentDto 수정할 내용이 담겨있는 dto
-     * @return 수정한 내용을 반영한 dto
-     * @author nayoung
-     */
-    @PutMapping
-    public ResponseEntity<CommentDto> updateComment(@RequestBody CommentDto commentDto) {
-        return ResponseEntity.ok(commentService.updateComment(commentDto));
-    }
+		return ResponseEntity.ok(commentDtoList);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteComment(@PathVariable Long id, @RequestBody DeleteCommentDto deleteCommentDto) {
+		Comment comment = commentRepository.findById(id)
+			.orElseThrow(() -> new RuntimeException("Comment not found"));
+
+		if (!comment.getUserId().equals(deleteCommentDto.getUserId())) {
+			return ResponseEntity.badRequest().body("User not authorized to delete this comment");
+		}
+
+		commentRepository.delete(comment);
+		return ResponseEntity.ok().build();
+	}
+
+	/**
+	 * 댓글 수정 controller
+	 * @param commentDto 수정할 내용이 담겨있는 dto
+	 * @return 수정한 내용을 반영한 dto
+	 * @author nayoung
+	 */
+	@PutMapping
+	public ResponseEntity<CommentDto> updateComment(@RequestBody CommentDto commentDto) {
+		return ResponseEntity.ok(commentService.updateComment(commentDto));
+	}
 }
--- src/main/java/com/ormi5/movieblog/commentcontroller/CommentRepository.java
+++ src/main/java/com/ormi5/movieblog/commentcontroller/CommentRepository.java
@@ -1,5 +1,7 @@
 package com.ormi5.movieblog.commentcontroller;
 
+import java.util.List;
+
 import com.ormi5.movieblog.comment.Comment;
 
 import org.springframework.stereotype.Repository;
@@ -7,7 +9,9 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ormi5.movieblog.comment.Comment;
+import com.ormi5.movieblog.post.Post;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+	List<Comment> findByPost(Post post);
 }
